1e1447f1114adc0b28405792e28a62113aa3fdf2
vgutta <vineethg@udel.edu>
2018-07-02 00:19:08 -0400

    functions

--- interface/src/App.js
+++ interface/src/App.js
@@ -102,7 +102,100 @@ class App extends Component {
     }
   }
 
+  handleResize () {
+    var currentData = this.state.currentData.slice();
+    var data = this.state.data.slice();
+    var currentUserScatterData = this.state.currentUserScatterData.slice();
+    var currentUserScatterColor = this.state.currentUserScatterColor.slice();
+    var currentMLScatterData = this.state.currentMLScatterData.slice();
+    var currentMLScatterColor = this.state.currentMLScatterColor.slice();
+    if (currentData.length > 0 && currentData.length === data.length) {
+      this.setState({ resizing: true });
+      this.plotGraph(
+        data,
+        currentData,
+        currentUserScatterData,
+        currentUserScatterColor,
+        currentMLScatterData,
+        currentMLScatterColor
+      );
+    }
+  }
+
+  handleSlider(event) {
+    this.setState({ sliderVal: event.target.value });
+  }
   
+  getNewStock() {
+    var data = [];
+    var randStock;
+    var stocks = this.state.stocks.slice();
+    var userStockData = this.state.userStockData;
+    var mlStockData = this.state.mlStockData;
+    async.series([
+      callback => {
+        randStock = stocks[Math.floor(Math.random() * stocks.length)];
+        console.log(randStock);
+        fetch("/getstockdata/?stock=" + randStock[1], {
+          method: "get"
+        })
+          .then(function(res) {
+            return res.json();
+          })
+          .then(function(response) {
+            data = JSON.parse(response).data;
+            callback();
+          });
+      },
+      callback => {
+        userStockData.finalStockValue = parseFloat(
+          data[data.length - 1].EOD * userStockData.currentStocks
+        );
+        this.setState({
+          data: data,
+          randStock: randStock,
+          currentData: [],
+          currentUserScatterData: [],
+          currentUserScatterColor: [],
+          currentMLScatterData: [],
+          currentMLScatterColor: [],
+          gettingNewStock: false,
+          userStockData: userStockData,
+          mlStockData: mlStockData
+        });
+        this.plotTimer();
+        callback();
+      }
+    ]);
+  }
+
+  handleStart() {
+    if (document.getElementById("start-btn").classList.contains("btn-active")) {
+      document.getElementById("start-btn").style.backgroundColor =
+        "rgb(142, 142, 142)";
+      document.getElementById("start-btn").classList.toggle("btn-active");
+      var currentData = this.state.currentData.slice();
+      var data = this.state.data.slice();
+      var userStockData = this.state.userStockData;
+      var mlStockData = this.state.mlStockData;
+      if (currentData.length === data.length) {
+        userStockData.currentStocks = userStockData.initialStocks;
+        userStockData.currentBuys = userStockData.initialBuys;
+        userStockData.currentSells = userStockData.initialSells;
+        userStockData.bank = 0;
+        mlStockData.currentStocks = mlStockData.initialStocks;
+        mlStockData.currentBuys = mlStockData.initialBuys;
+        mlStockData.currentSells = mlStockData.initialSells;
+        mlStockData.bank = 0;
+        this.setState({
+          gettingNewStock: true,
+          userStockData: userStockData,
+          mlStockData: mlStockData
+        });
+        this.getNewStock();
+      }
+    }
+  }
 }
 
 export default App;
